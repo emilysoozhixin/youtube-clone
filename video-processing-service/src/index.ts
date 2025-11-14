d79d6c748b3c8fa8e1b3f17feaa13cd33c43e6d0
Emily <zhi.soo@kcl.ac.uk>
2025-11-14 14:53:03 +0000

    Process video locally

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,32 @@
 import express from "express";
+import Ffmpeg from "fluent-ffmpeg";
+
 
 const app = express();
-const port = 3000;
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Hello Word!");
+app.post("/process-video", (req, res) => {
+    // Get path of the input video file from the req body 
+    const inputFilePath = req.body.inputFilePath;
+    const ouputFilePath = req.body.ouputFilePath;
+
+    if (!inputFilePath || !ouputFilePath) {
+        res.status(400).send("Bad Request: Missing file path. ");
+    }
+
+    Ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360") // 360p
+        .on("end", () => {
+            res.status(200).send("Processing finished successfully.")
+        })
+        .on("error", (err) => {
+            console.log(`An error occured: ${err.message}`);
+            res.status(500).send(`Internal Server Error: ${err.message}`)
+        })
+        .save(ouputFilePath)
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(
         `Video processing service listening at http://localhost:${port}`);
